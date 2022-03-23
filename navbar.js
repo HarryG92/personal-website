1b03de2defa39a8f410f4951b96b9f204196cefc
H Gulliver <harry.gulliver@gmail.com>
2022-03-23 20:18:46 +0000

    fleshing out basic structure

---
+++ navbar.js
@@ -0,0 +1,22 @@
+let navlinks = {
+    "Home": "index.html",
+    "Qualifications": "quals.html",
+    "Work Experience": "work_experience.html",
+    "Publications": "publications.html",
+    "Coding Skills": "coding.html",
+    "Mathematics": "maths.html",
+    "Fencing": "fencing.html"
+}
+
+let navbar = document.createElement("div");
+navbar.id = "navbar";
+
+for (const [key, value] of Object.entries(navlinks)) {
+    let a = document.createElement("a");
+    a.innerHTML = key;
+    a.href = value;
+    a.id = key;
+    navbar.appendChild(a);
+}
+
+document.body.insertBefore(navbar, document.body.childNodes[0]);
