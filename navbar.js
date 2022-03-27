d3c16c8661cc31b6c732845d5d6f0d5523db718c
H Gulliver <harry.gulliver@gmail.com>
2022-03-27 18:55:55 +0100

    begun styling navbar

--- navbar.js
+++ navbar.js
@@ -4,18 +4,32 @@ let navlinks = {
     "Work Experience": "work_experience.html",
     "Technical Skills": "technical.html",
     "Mathematics": "maths.html",
-    "Fencing": "fencing.html"
+    // "Fencing": "fencing.html"
 }
+
+let path = window.location.pathname;
+let page = path.split("/").pop();
 
 let navbar = document.createElement("div");
 navbar.id = "navbar";
 
 for (const [key, value] of Object.entries(navlinks)) {
-    let a = document.createElement("a");
-    a.innerHTML = key;
-    a.href = value;
-    a.id = key;
-    navbar.appendChild(a);
+    let b = document.createElement("button");
+    b.innerText = key;
+    // b.href = value;
+    b.id = key;
+    if (value === page) {
+        b.classList.add("current-page");
+    }
+    navbar.appendChild(b);
+    b.onclick = redirectFactory(value);
 }
 
 document.body.insertBefore(navbar, document.body.childNodes[0]);
+
+function redirectFactory(value) {
+    const redirect = function() {
+        window.location.href = value;
+    }
+    return redirect;
+}
