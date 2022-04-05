ba553efceb95d4255e2975c031becf06f664e688
H Gulliver <harry.gulliver@gmail.com>
2022-04-05 18:47:33 +0100

    put navlinks in json file and reading in; need to reformat json to suit purposes of both navbar and express

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const navlinks = require('./public/assets/navlinks.json');
+console.log(navlinks);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-app.get('/', (req, res) => {
-    res.render('index', {page: "H Gulliver, PhD"});
-});
+
+for (let [key, value] of Object.entries(navlinks)) {
+    app.get(value, (req, res) => {
+        res.render(value, {page: key});
+    });
+}
+
+// app.get('/', (req, res) => {
+//     res.render('index', {page: "H Gulliver, PhD"});
+// });
 
 app.get('*', (req, res) => {
     res.send("These aren't the droids you're looking for!");
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,106 +1,122 @@
-let navlinks = {
-    "Home": "index.html",
-    "Qualifications": "quals.html",
-    "Work Experience": "work_experience.html",
-    "Technical Skills": "technical.html",
-    "Mathematics": "maths.html",
-    // "Fencing": "fencing.html"
-}
+// let navlinks = {
+//     "Home": "/",
+//     "Qualifications": "quals",
+//     "Work Experience": "work_experience",
+//     "Technical Skills": "technical",
+//     "Mathematics": "maths",
+//     // "Fencing": "fencing"
+// }
 
-let path = window.location.pathname;
-let currentPage = path.split("/").pop();
-fitNavbar();
+// const navlinks = require('./../assets/navlinks.json');
+fetch('./../assets/navlinks.json')
+    .then(
+        (response) => {
+            return response.json();
+        }
+    )
+    .then(
+        (navlinks) => {
+            main(navlinks);
+        }
+    )
 
-window.addEventListener('resize', fitNavbar);
+function main(navlinks) {
+    let path = window.location.pathname;
+    let currentPage = path.split("/").pop();
 
-function fitNavbar(event) {
-    let oldNavbar = document.getElementById("navbar");
-    if (oldNavbar != null) {
-        oldNavbar.remove();
-    }
-    let width = window.innerWidth;
-    if (width < 850) {
-        condensedNavbar();
-    } else {
-        fullNavbar();
-    }
-}
+    fitNavbar();
+    window.addEventListener('resize', fitNavbar);
 
-function fullNavbar() {
-    let navbar = document.createElement("div");
-    navbar.id = "navbar";
-
-    for (const [key, value] of Object.entries(navlinks)) {
-        let button = createButton(key, value);
-        navbar.appendChild(button);
-    }
-
-    document.body.insertBefore(navbar, document.body.childNodes[0]);
-}
-
-function condensedNavbar() {
-    let navbar = document.createElement("div");
-    navbar.id = "navbar";
-
-    let menuIcon = createMenuIcon();
-    menuIcon.onmouseover = revealMenu;
-    navbar.appendChild(menuIcon);
-
-    let dropdown = document.createElement("div");
-    dropdown.id = "dropdown";
-
-    for (const [key, value] of Object.entries(navlinks)) {
-        let button = createButton(key, value);
-        if (key === "Home" || value === currentPage) {
-            navbar.appendChild(button);
+    function fitNavbar(event) {
+        let oldNavbar = document.getElementById("navbar");
+        if (oldNavbar != null) {
+            oldNavbar.remove();
+        }
+        let width = window.innerWidth;
+        if (width < 850) {
+            condensedNavbar();
         } else {
-            dropdown.appendChild(button);
+            fullNavbar();
         }
     }
 
-    navbar.onmouseleave = hideMenu;
-    navbar.appendChild(dropdown);
+    function fullNavbar() {
+        let navbar = document.createElement("div");
+        navbar.id = "navbar";
 
-    document.body.insertBefore(navbar, document.body.childNodes[0]);
-}
+        for (let [key, value] of Object.entries(navlinks)) {
+            let button = createButton(key, value);
+            navbar.appendChild(button);
+        }
 
-function redirectFactory(value) {
-    const redirect = function() {
-        window.location.href = value;
-    }
-    return redirect;
-}
-
-function createButton(text, page) {
-    let b = document.createElement("button");
-    b.innerText = text;
-    b.id = text;
-    if (page === currentPage) {
-        b.classList.add("current-page");
-    }
-    b.onclick = redirectFactory(page);
-    return b;
-}
-
-function createMenuIcon() {
-    let menuIcon = document.createElement("div");
-    menuIcon.id = "menu-icon";
-
-    for (var i = 0; i < 3; i++) {
-        let bar = document.createElement("div");
-        bar.classList.add("menu-bar");
-        menuIcon.appendChild(bar);
+        document.body.insertBefore(navbar, document.body.childNodes[0]);
     }
 
-    return menuIcon;
-}
+    function condensedNavbar() {
+        let navbar = document.createElement("div");
+        navbar.id = "navbar";
 
-function revealMenu() {
-    let dropdown = document.getElementById("dropdown");
-    dropdown.classList.add("dropdown-reveal");
-}
+        let menuIcon = createMenuIcon();
+        menuIcon.addEventListener('mouseover', revealMenu);
+        navbar.appendChild(menuIcon);
 
-function hideMenu() {
-    let dropdown = document.getElementById("dropdown");
-    dropdown.classList.remove("dropdown-reveal");
+        let dropdown = document.createElement("div");
+        dropdown.id = "dropdown";
+
+        for (const [key, value] of Object.entries(navlinks)) {
+            let button = createButton(key, value);
+            if (key === "Home" || value === currentPage) {
+                navbar.appendChild(button);
+            } else {
+                dropdown.appendChild(button);
+            }
+        }
+
+        navbar.addEventListener('mouseleave', hideMenu);
+        navbar.appendChild(dropdown);
+
+        document.body.insertBefore(navbar, document.body.childNodes[0]);
+    }
+
+    function redirectFactory(value) {
+        const redirect = function() {
+            window.location.href = value;
+        }
+        return redirect;
+    }
+
+    function createButton(text, page) {
+        let b = document.createElement("button");
+        b.innerText = text;
+        b.id = text;
+        if (page === currentPage) {
+            b.classList.add("current-page");
+        }
+        b.addEventListener('click', redirectFactory(page));
+        return b;
+    }
+
+    function createMenuIcon() {
+        let menuIcon = document.createElement("div");
+        menuIcon.id = "menu-icon";
+
+        for (var i = 0; i < 3; i++) {
+            let bar = document.createElement("div");
+            bar.classList.add("menu-bar");
+            menuIcon.appendChild(bar);
+        }
+
+        return menuIcon;
+    }
+
+    function revealMenu() {
+        let dropdown = document.getElementById("dropdown");
+        dropdown.classList.add("dropdown-reveal");
+    }
+
+    function hideMenu() {
+        let dropdown = document.getElementById("dropdown");
+        dropdown.classList.remove("dropdown-reveal");
+    }
+
 }
