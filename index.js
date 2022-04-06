719d1fc0f36483f83c0571bacd6d51119205802c
H Gulliver <harry.gulliver@gmail.com>
2022-04-06 13:56:09 +0100

    configured port for heroku

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('*', (req, res) => {
     res.send("These aren't the droids you're looking for!");
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
 });
